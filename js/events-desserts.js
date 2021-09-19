e12b0233cc239f623144836324c176a85fa5d7ca
MariaLuque <maritcarmn@gmail.com>
2021-09-19 11:24:27 -0500

    Add js with events

---
+++ js/events-desserts.js
@@ -0,0 +1,20 @@
+let menu = document.getElementById("hamburger");
+menu.addEventListener("click", function() {
+    let nav = document.getElementById("nav");
+    if (nav.style.display == "none") {
+        nav.style.display = "flex";
+    } else {
+        nav.style.display = "none";
+    }
+
+});
+
+let perfil = document.getElementById("btn-perfil");
+perfil.addEventListener("click", function() {
+    let hd_perfil = document.getElementById("header-perfil");
+    if (hd_perfil.style.display == "none") {
+        hd_perfil.style.display = "flex";
+    } else {
+        hd_perfil.style.display = "none";
+    }
+});
