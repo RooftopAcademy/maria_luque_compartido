28f547a72aa5f4e26e36fe64cf9b65869026001e
MariaLuque <maritcarmn@gmail.com>
2021-09-27 12:36:19 -0500

    Add register and chenges in events-desserts

--- js/events-desserts.js
+++ js/events-desserts.js
@@ -1,3 +1,9 @@
+/*
+-----MOUSE EVENTS------
+*/
+
+//-----CLICK EVENTS IN IMG PROFILE AND HAMBURGER MENU
+
 let menu = document.getElementById("hamburger");
 menu.addEventListener("click", function() {
     let nav = document.getElementById("nav");
@@ -11,8 +17,6 @@ menu.addEventListener("click", function() {
 
 let perfil = document.getElementById("btn-perfil");
 perfil.addEventListener("click", function() {
-    // let imagen = this.getElementsByTagName("img");
-    // imagen.setAttribute("src", "img/login-blanco.jpg");
     let hd_perfil = document.getElementById("header-perfil");
 
     if (hd_perfil.style.display == "none") {
@@ -21,3 +25,24 @@ perfil.addEventListener("click", function() {
         hd_perfil.style.display = "none";
     }
 });
+
+//---- MOUSEOVER AND MOUSEOUT EVENT IN MENU ITEMS
+
+let menuItem = document.querySelectorAll(".menu-item");
+
+menuItem.forEach(element => {
+    element.addEventListener('mouseover', function() {
+        element.style.backgroundColor = "#FF9F1C";
+        element.style.textShadow = "2px 2px 2px rgba(0,0,0,0.5)";
+    })
+});
+
+menuItem.forEach(element => {
+    element.addEventListener('mouseout', function() {
+        element.style.backgroundColor = "#c98749";
+        element.style.textShadow = "";
+    })
+});
+
+
+/*  ----EVENTOS DE INFORMACIÓN */
