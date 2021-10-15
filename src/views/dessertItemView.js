c9598fdfb0dae17785be615bfc55b80a09ac035c
MariaDelCarmenLuque <maritcarmn@gmail.com>
2021-10-15 01:23:14 -0500

    Issue-12 Add desserts to Wishes list

--- src/views/dessertItemView.js
+++ src/views/dessertItemView.js
@@ -7,7 +7,7 @@ function dessertItemView(dessert) {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<a class="js-view-more" href="detail-dessert.html">View more</a></p>
             <div class ="d-flex card-actions">
                 <button class=" btn-action-buy js-add-to-cart" data-dessert-id="${dessert.id}"><img src = "img/add_buy_icon.png"> </button>
-                <button class=" btn-action-wishes"><img src="img/add_like.png" > </button>
+                <button class=" btn-action-wishes js-add-to-wishes-list"><img src="img/add_like.png" > </button>
                 <button class="btn-card-price">$ ${dessert.price} </button>
             </div>
         </div>
